58f3dc5ac535005b73ed38e257653a91cd0f9746
fedio <feditrabelsi08@gmail.com>
2021-12-16 11:41:27 +0100

    Configs

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        
+        'headfoot': '#222222',
+        'gray' : '#F2F2F2',
+        'beige': '#A88C6A',
+        'font-gray' : '#b6b6b6',
+        'header':'#707070',
+        
+      },
+    },
+  },
+  plugins: [],
+}
